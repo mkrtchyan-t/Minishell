913c3c76fd518591c7c627da8394d05793d62ae3
Ara Stepanyan <arastepa@c1r2s6.42yerevan.am>
2021-07-14 21:08:42 +0400

    minishell

--- checkredirects.c
+++ checkredirects.c
@@ -13,38 +13,36 @@ void 	checkredirs(t_redirs *redir, char *line)
 	t_redirs 	*new;
 
 	redir = NULL;
-	new = malloc(sizeof(t_redirs));
-	i = 0;
+	new = NULL;
 	j = 0;
-	while (ft_strchr((line + i), '>'))
-	{
-		initredirs(new);
-		new->outfile = ft_strdup("");
+	i = 0;
+	while (line[i] != '\0')
+	{	
 		j = 0;
-		while (line[i] != '\0')
+		if ((line[i] == '>' && line[i + 1] == '>') || \
+			(line[i] == '>' && line[i + 1] != '>'))
 		{
-			if ((line[i] == '>' && line[i + 1] == '>') || \
-				(line[i] == '>' && line[i + 1] != '>'))
+			new = malloc(sizeof(t_redirs));
+			initredirs(new);
+			new->redir = 1;
+			new->outfile = ft_strdup("");
+			while (line[i] == '>' && line[i])
+				i++;
+			while (ft_isspace(line[i]) && line[i])
+				i++;
+			while (!ft_isspace(line[i]) && line[i] != '>' && line[i])
 			{
-				new->redir = 1;
-				while (line[i] == '>' && line[i])
-					i++;
-				while (ft_isspace(line[i]) && line[i])
-					i++;
-				while (!ft_isspace(line[i]) && line[i] != '>' && line[i])
-				{
-					new->outfile[j++] = line[i];
-					i++;
-				}
+				new->outfile[j++] = line[i];
+				i++;
 			}
-			i++;
+			i--;
 		}
-		if (ft_strcmp(new->outfile, "") != 0)
+		if (new)
 		{
-			addback(&redir, new);
-			printf("%s", redir->outfile);
+			if (ft_strcmp(new->outfile, "") != 0)
+				addback(&redir, new);
 		}
-		new = malloc(sizeof(t_redirs));
+		new = NULL;
+		i++;
 	}
-	printf("%s", redir->next->outfile);
 }
